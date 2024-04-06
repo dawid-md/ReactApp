6d82478aa99921968d3130ebc6785153737c1f4e
Mądrzyk <maddawid@gmail.com>
2024-04-06 18:28:22 +0200

    test changes vs azure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Some nice logo!
         </p>
         <a
           className="App-link"
