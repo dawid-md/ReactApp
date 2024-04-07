1b9e0ceb3d2fb88fd911949201ff1c513f44b66b
Mądrzyk <maddawid@gmail.com>
2024-04-08 01:01:52 +0200

    new version with routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import MyTasks from './pages/MyTasks/MyTasks';
 
 function App() {
-
-  const [user , setUser] = useState(null)
-  const [loading , setLoading] = useState(false)
 
   return (
     <div className="App">
