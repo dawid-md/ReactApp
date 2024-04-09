0d944875c543ba118d726506f042c46bd8f97af0
Mądrzyk <maddawid@gmail.com>
2024-04-09 22:24:54 +0200

    connection to azure cosmos db

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import MyTasks from './pages/MyTasks/MyTasks';
+import { CosmosClient } from '@azure/cosmos';
 
 function App() {
+
+  const endpoint = process.env.REACT_APP_COSMOS_ENDPOINT;
+  const key = process.env.REACT_APP_COSMOS_KEY;
+  const client = new CosmosClient({ endpoint, key });
+
+  const database = client.database("cctasks");
+  const container = database.container("ccdb2024");
+
+  // Example function to query items
+  async function queryItems() {
+    const { resources: items } = await container.items
+      .query("SELECT * from c")
+      .fetchAll();
+    console.log(items);
+  }
+
+  queryItems();
 
   return (
     <div className="App">
