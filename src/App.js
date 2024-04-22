fb06a537141e2ecce33cbd6d3186a3b2806c4d5e
Mądrzyk <maddawid@gmail.com>
2024-04-22 23:59:24 +0200

    azure crud

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import MyTasks from './pages/MyTasks/MyTasks';
 import { CosmosClient } from '@azure/cosmos';
+import { useEffect, useState } from 'react';
+
+const newItem = {
+  name: 'Do the laundry',
+  assignedTo: 'John Doe',
+}
+
+const updatedFields = {
+  id: '01',
+  name: 'Do the bjork',
+  assignedTo: 'Jane'
+}
 
 function App() {
   const endpoint = process.env.REACT_APP_COSMOS_ENDPOINT
   const key = process.env.REACT_APP_COSMOS_KEY
   const client = new CosmosClient({ endpoint, key })
+  const [items, setItems] = useState([])
 
   const database = client.database("cctasks")
   const container = database.container("ccdb2024")
@@ -16,22 +29,53 @@ function App() {
     const { resources: items } = await container.items
       .query("SELECT * from c")
       .fetchAll();
-    console.log(items)
+      setItems(items)
+      console.log(items)
   }
 
-  queryItems();
+  async function createItem(newItem) {
+    try {
+        const { resource: createdItem } = await container.items.create(newItem);
+        // setItems(prevItems => [...prevItems, createdItem]);
+        queryItems()
+    } catch (error) {
+        console.error('Error adding new item:', error);
+    }
+  }
+
+  async function updateItem(itemId, updatedFields) {
+    try {
+        const { resource: updatedItem } = await container.item(itemId).replace(updatedFields);
+        setItems(prevItems => prevItems.map(item => item.id === itemId ? updatedItem : item));
+    } catch (error) {
+        console.error('Error updating item:', error);
+    }
+  } 
+
+  async function deleteItem(itemId) {
+    try {
+        await container.item(itemId).delete();
+        setItems(prevItems => prevItems.filter(item => item.id !== itemId));
+    } catch (error) {
+        console.error('Error deleting item:', error);
+    }
+  }
+
+  useEffect(() => {
+    queryItems()
+  }, [])
 
   return (
-    <div className="App">
       <BrowserRouter>
-        <div className="page-content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home items={items} />} />
             <Route path="/mytasks" element={<MyTasks />} />
           </Routes>
-        </div>
+          <button onClick={queryItems}>Query Items</button>
+          <button onClick={() => createItem(newItem)}>Create Item</button>
+          <button onClick={() => deleteItem('01')}>Delete Item</button>
+          <button onClick={() => updateItem('1c62ccea-2d7a-4b91-819f-ee930a6beca3', updatedFields)}>Update Item</button>
       </BrowserRouter>
-    </div>
   );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,17 @@
-export default function Home() {
+export default function Home({ items }) {
+    console.log(items);
+
     return (
-        <h1>This is home, wellcome!</h1>
+        <div className="home-container">
+            <div className="left-panel"></div>
+            <div className="home-content">
+                {items.map((item) => (
+                    <div key={item.id} className="task">
+                        <h3>{item.id}</h3>
+                        <p>{item.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
